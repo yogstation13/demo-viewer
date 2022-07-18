267edd12b89aa43dcd185a890ae10df243ca23ca
monster860 <mnmaxim@gmail.com>
2022-07-18 05:37:50 -0400

    readd some files

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,7 @@ import nodeResolve from "rollup-plugin-node-resolve";
 import postcss from 'rollup-plugin-postcss';
 import { terser } from "rollup-plugin-terser";
 import OMT from "@surma/rollup-plugin-off-main-thread";
+import {readFileSync} from "fs";
 
 rimraf.sync("dist");
 
@@ -34,7 +35,25 @@ const config = {
 		}),
 		OMT(),
 		htmlPlugin(),
-		//terser()
+		terser(),
+		{
+			name: 'files-plugin',
+			buildStart() {
+				this.addWatchFile('CNAME');
+			},
+			async generateBundle(options, bundle) {
+				this.emitFile({
+					fileName: 'CNAME',
+					type: 'asset',
+					source: readFileSync('CNAME', 'utf8')
+				});
+				this.emitFile({
+					fileName: 'favicon.ico',
+					type: 'asset',
+					source: readFileSync('favicon.ico', 'utf8')
+				});
+			}
+		}
 	]
 };
 export default config;
